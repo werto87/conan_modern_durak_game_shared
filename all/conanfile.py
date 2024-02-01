77f0e3087869f71d21ce6e6bf1223c29b88f2769
werto87 <huhulasmichinruhe@web.de>
2024-02-01 16:44:40 +0900

    Initial commit

---
+++ all/conanfile.py
@@ -0,0 +1,33 @@
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.files import get
+
+required_conan_version = ">=1.51.1"
+
+
+class ConanHeaderOnly(ConanFile):
+    name = "lib_name"
+    package_type = "header-library"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def configure(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, "20")
+        self.options["boost"].header_only = True
+        self.options["fmt"].header_only = True
+
+    def requirements(self):
+        self.requires("boost/1.84.0")
+
+    def layout(self):
+        cmake_layout(self, src_folder=self.name + "-" + str(self.version))
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.install()
